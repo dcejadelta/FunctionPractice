a61e9e4af66b97297115731474e914ae70501514
dcejadelta <deana.ceja@deltacollege.edu>
2025-12-05 00:16:49 +0000

    added two functions: evil_function and good_function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+def evil_function():
+    print("Muahaha, behold, visitor! It is I: The Evil Function!")
+    print("You have made a grave mistake entering my domain...")
 
+
+def good_function():
+    print("Welcome, esteemed guest. You have made it to the Good Function.")
+    print("This is my humble abode. Please make yourself at home. But remember to stay away from my twin, Evil Function.")
+
+def main():
+    print("Hi this is a test")
+    good_function()
+    evil_function()
+
+main()
